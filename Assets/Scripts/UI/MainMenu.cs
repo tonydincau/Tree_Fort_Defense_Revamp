6b5f9767851035abaaaf4e17b5b3b51e69ab3823
Feddas <Feddas00@gmail.com>
2013-11-02 20:35:04 -0700

    Added MainMenu with Start button

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+		//Escape on Android is the back button
+		if (Input.GetKeyDown(KeyCode.Escape))
+			Application.Quit();	
+	}
+	
+	public void OnClickStart()
+	{
+		//Load Level_001
+		Application.LoadLevel(1);
+	}
+}
